d7bfe2eb1745ff3b18eda45961710103d24f0bcb
Troy Barbour <tmbarbour@gmail.com>
2021-01-28 01:03:16 -0500

    update file name

--- install_completion_script.sh
+++ install_completion_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Copying the reef-pi completion script to /usr/share/bash-completion/completions/reef-pi"
-sudo cp _reefpi_db_completion.sh /usr/share/bash-completion/completions/reef-pi
-source /usr/share/bash-completion/completions/reef-pi
+echo "Copying the reef-pi completion script to /usr/share/bash-completion/completions/_reef-pi"
+sudo cp _reef-pi /usr/share/bash-completion/completions/
+source /usr/share/bash-completion/completions/_reef-pi
 
 echo "Completion script installed"
